5a31d7d3d0b2ba7d27a5f1210cd62174f2de938f
Dan Habot <dan.habot@gmail.com>
2020-07-18 17:39:01 -0400

    Added header to panel

--- app/src/panel.js
+++ app/src/panel.js
@@ -1,46 +1,52 @@
-import React from 'react'
-import {
-  useParams
-} from "react-router-dom"
-import { connect } from 'react-redux'
-import { Grid, Box, Button, Text, AspectRatio, Heading } from 'theme-ui'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { connect } from "react-redux";
+import { Grid, Box, Button, Text, AspectRatio, Heading, Divider } from "theme-ui";
 
 function Panel(props) {
-  let { category } = useParams()
-  let buttons = props.categories[category]
+  let { category } = useParams();
+  let buttons = props.categories[category];
 
   if (buttons) {
     return (
-      <Grid columns={props.columns} gap={4}>
-        {buttons.map(button => {
-          return (<Box>
-            <AspectRatio ratio={1}>
-              <Button variant="panel" sx={{
-                backgroundColor: button.color,
-                height: '100%',
-                width: '100%',
-                '&:hover': {
-                  filter: 'brightness(150%)'
-                }
-              }}>
-                <Heading>{button.title}</Heading>
-              </Button>
-            </AspectRatio>
-          </Box>)
-        })}
-      </Grid>
-    )
-  }
-  else {
-    return (<Text>No Buttons</Text>)
+      <Box>
+        <Heading as="h1">{category}</Heading>
+        <Divider />
+        <Grid columns={props.columns} gap={2}>
+          {buttons.map((button) => {
+            return (
+              <Box>
+                <AspectRatio ratio={1}>
+                  <Button
+                    variant="panel"
+                    sx={{
+                      backgroundColor: button.color,
+                      height: "100%",
+                      width: "100%",
+                      "&:hover": {
+                        filter: "brightness(150%)",
+                      },
+                    }}
+                  >
+                    <Heading>{button.title}</Heading>
+                  </Button>
+                </AspectRatio>
+              </Box>
+            );
+          })}
+        </Grid>
+      </Box>
+    );
+  } else {
+    return <Text>No Buttons</Text>;
   }
 }
 
 function mapStateToProps(store) {
   return {
     categories: store.categoriesState.categories,
-    columns: store.appState.columns
-  }
+    columns: store.appState.columns,
+  };
 }
 
-export default connect(mapStateToProps)(Panel)
+export default connect(mapStateToProps)(Panel);

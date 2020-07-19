a0e18198dab9ef7c4a9f100fd6a5ee21ac4a303f
Dan Habot <dan.habot@gmail.com>
2020-07-18 20:52:30 -0400

    Enabled service worker

--- app/src/panel.js
+++ app/src/panel.js
@@ -24,6 +24,7 @@ function Panel(props) {
                       backgroundColor: button.color,
                       height: "100%",
                       width: "100%",
+                      borderRadius: '20px',
                       "&:hover": {
                         filter: "brightness(150%)",
                       },
